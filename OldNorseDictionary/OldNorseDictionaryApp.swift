07c31a807cc9711d23c0e723bd3604455a49023e
Andrew Skurlatov <a.skurlatov@gmail.com>
2023-07-14 15:31:25 +0200

    Initial Commit

---
+++ OldNorseDictionary/OldNorseDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldNorseDictionaryApp.swift
+//  OldNorseDictionary
+//
+//  Created by Andrey Skurlatov on 14.7.23..
+//
+
+import SwiftUI
+
+@main
+struct OldNorseDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
